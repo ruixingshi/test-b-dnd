d8a9b1b7c2d5a8217fecd8e97c6465c72aebe1be
shiruixing <shiruixing@meituan.com>
2024-08-14 09:52:16 +0800

    feat: demo 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-import React, { useState, useRef , useEffect} from "react";
+import React, { useState, useRef } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragOutlined } from "@ant-design/icons";
 import "./App.css";
+
+const CHILD_COUNTS = 3;
+
+const getChildItems = (count, fatherKey) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `cItem-${fatherKey * (CHILD_COUNTS + 1) + 1 + k}`,
+    content: `cItem-${fatherKey * (CHILD_COUNTS + 1) + 1 + k}`,
+    group: fatherKey,
+    index: fatherKey * (CHILD_COUNTS + 1) + 1 + k,
+  }));
 
 const getItems = (count) =>
   Array.from({ length: count }, (v, k) => k).map((k) => ({
-    id: `item-${k}`,
-    content: `item ${k}`,
+    group: k,
+    id: `item-${k * (CHILD_COUNTS + 1)}`,
+    content: `item ${k * (CHILD_COUNTS + 1)}`,
+    child: getChildItems(CHILD_COUNTS, k),
+    index: k * (CHILD_COUNTS + 1),
   }));
 
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-  return result;
-};
+// const reorder = (list, startIndex, endIndex) => {
+//   const result = Array.from(list);
+//   const [removed] = result.splice(startIndex, 1);
+//   result.splice(endIndex, 0, removed);
+//   return result;
+// };
 
 const getItemStyle = (isDragging, draggableStyle) => ({
   border: "solid 2px green",
@@ -28,33 +42,81 @@ const App = () => {
   const appRef = useRef(null);
 
   const onDragEnd = (result) => {
+    console.log("@@@ result", result);
     if (!result.destination) {
       return;
     }
-    const newItems = reorder(
-      items,
-      result.source.index,
-      result.destination.index
-    );
-    setItems(newItems);
+    // const newItems = reorder(
+    //   items,
+    //   result.source.index,
+    //   result.destination.index
+    // );
+    // setItems(newItems);
   };
 
-  // 监听滚动事件
-  useEffect(() => {
-    const handleScroll = (e) => {
-      console.log("@@@ handleScroll", e);
-    };
-    appRef.current.addEventListener("scroll", handleScroll);
-    return () => {
-      appRef.current.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const renderDragGroup = (item, index) => {
+    return (
+      <div className="group-item" id={index}>
+        <Draggable
+          key={item?.id}
+          draggableId={item?.id}
+          index={index * (CHILD_COUNTS + 1)}
+        >
+          {(provided, snapshot) => (
+            <div
+              className="group-item-drag"
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              style={getItemStyle(
+                snapshot.isDragging,
+                provided.draggableProps.style
+              )}
+            >
+              <div {...provided.dragHandleProps}>
+                <DragOutlined />
+              </div>
+              {"分组" + index + "内容" + item.content}
+            </div>
+          )}
+        </Draggable>
+      </div>
+    );
+  };
+
+  const renderDragChild = (cItem, cIndex, index) => {
+    return (
+      <div className="child-item">
+        <Draggable
+          key={cItem.id}
+          draggableId={cItem.id}
+          index={index * (CHILD_COUNTS + 1) + 1 + cIndex}
+        >
+          {(provided, snapshot) => (
+            <div
+              className="child-item-drag"
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              style={getItemStyle(
+                snapshot.isDragging,
+                provided.draggableProps.style
+              )}
+            >
+              <div {...provided.dragHandleProps}>
+                <DragOutlined />
+              </div>
+              {cItem.content}
+            </div>
+          )}
+        </Draggable>
+      </div>
+    );
+  };
 
   return (
     <div className="app">
       <button
         onClick={() => {
-          console.log("@@@ appRef", appRef.current);
+          appRef.current.scrollTop = 0;
         }}
       >
         滚动回来
@@ -71,21 +133,16 @@ const App = () => {
               }}
             >
               {items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
+                <>
+                  {/* 分组可拖拽 */}
+                  {renderDragGroup(item, index)}
+                  {/* 列表项可拖拽 */}
+                  <div className="child-container">
+                    {item.child.map((cItem, cIndex) =>
+                      renderDragChild(cItem, cIndex, index)
+                    )}
+                  </div>
+                </>
               ))}
               {provided.placeholder}
             </div>
---
+++ src/AppGoal.tsx
@@ -0,0 +1,218 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+import React, { useState, useRef, useMemo } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragOutlined, DownOutlined } from "@ant-design/icons";
+import { GROUP_INFO, GOALS_INFO } from "./mockData";
+import "./AppGoal.css";
+
+const CHILD_COUNTS = 3;
+
+const RENDER_INFO = () => {
+  const groupInfo = [...GROUP_INFO];
+  groupInfo.forEach((item) => {
+    const { startIndex, endIndex } = item;
+    // 从GOALS_INFO中获取对应的goal信息
+    item.childGoals = GOALS_INFO.slice(startIndex, endIndex);
+  });
+  return groupInfo;
+};
+
+const GROUP_FOLD_INFO = () => {
+  const groupInfo = {};
+  GROUP_INFO.forEach((item) => {
+    groupInfo[item.groupId] = false;
+  });
+  return groupInfo;
+};
+
+const getChildItems = (count, fatherKey) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `cItem-${fatherKey * (CHILD_COUNTS + 1) + 1 + k}`,
+    content: `cItem-${fatherKey * (CHILD_COUNTS + 1) + 1 + k}`,
+    group: fatherKey,
+    index: fatherKey * (CHILD_COUNTS + 1) + 1 + k,
+  }));
+
+const getItems = (count) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    group: k,
+    id: `item-${k * (CHILD_COUNTS + 1)}`,
+    content: `item ${k * (CHILD_COUNTS + 1)}`,
+    child: getChildItems(CHILD_COUNTS, k),
+    index: k * (CHILD_COUNTS + 1),
+  }));
+
+// const reorder = (list, startIndex, endIndex) => {
+//   const result = Array.from(list);
+//   const [removed] = result.splice(startIndex, 1);
+//   result.splice(endIndex, 0, removed);
+//   return result;
+// };
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  border: "solid 2px green",
+  margin: "2px",
+  ...draggableStyle,
+});
+
+const AppGoal = () => {
+  const [childGoalsgroupInfo, setGroupInfo] = useState(GROUP_INFO);
+  const [goalsInfo, setGoalsInfo] = useState(GOALS_INFO);
+  const [renderInfo, setRenderInfo] = useState(RENDER_INFO);
+  const [groupFoldStatus, setGroupFoldStatus] = useState(GROUP_FOLD_INFO());
+
+  // 计算所有的分组和目标的index，用于dnd的拖拽
+  const IndexArr = useMemo(() => {
+    const newArr = [];
+    renderInfo.forEach((item) => {
+      const { groupId, childGoals } = item;
+      newArr.push(String(groupId));
+      childGoals.forEach((_item) => {
+        const { goalSerialNumber } = _item;
+        if (goalSerialNumber) {
+          newArr.push(goalSerialNumber);
+        }
+      });
+    });
+    return newArr;
+  }, [renderInfo]);
+
+  const appRef = useRef(null);
+
+  const onDragEnd = (result) => {
+    console.log("@@@ result", result);
+    if (!result.destination) {
+      return;
+    }
+    // const newItems = reorder(
+    //   items,
+    //   result.source.index,
+    //   result.destination.index
+    // );
+    // setItems(newItems);
+  };
+
+  const onDragStart = (startItem) => {
+    console.log("@@@ start", startItem, startItem.draggableId.length);
+    // 如果拖拽的是分组
+    if (startItem.draggableId.length === 4) {
+      const newFoldStatus = { ...groupFoldStatus };
+      // 将所有分组状态设置为false
+      Object.keys(newFoldStatus).forEach((key) => {
+        newFoldStatus[key] = true;
+      });
+      setGroupFoldStatus(newFoldStatus);
+    }
+  };
+
+  const getInd = (_id) => {
+    return IndexArr.indexOf(String(_id));
+  };
+
+  return (
+    <div className="app">
+      <button
+        onClick={() => {
+          appRef.current.scrollTop = 0;
+        }}
+      >
+        滚动回来
+      </button>
+      <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div
+              className="app-container"
+              {...provided.droppableProps}
+              ref={(ref) => {
+                appRef.current = ref;
+                provided.innerRef(ref);
+              }}
+            >
+              {renderInfo.map((item) => {
+                return (
+                  <>
+                    {/* 分组标题 */}
+                    <Draggable
+                      key={String(item?.groupId)}
+                      draggableId={String(item?.groupId)}
+                      index={getInd(item?.groupId)}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          className="group-container"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          <div {...provided.dragHandleProps}>
+                            <DragOutlined />
+                          </div>
+                          <DownOutlined
+                            onClick={() => {
+                              const newFoldStatus = { ...groupFoldStatus };
+                              newFoldStatus[item.groupId] =
+                                !newFoldStatus[item.groupId];
+                              setGroupFoldStatus(newFoldStatus);
+                            }}
+                          />
+                          <div className="name group-name">
+                            {item.groupName}({item.goalCount})
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                    {/* 所有子目标 */}
+                    <div
+                      className={
+                        groupFoldStatus[item.groupId]
+                          ? "goals-fold"
+                          : "goals-expand"
+                      }
+                    >
+                      {item.childGoals.map((item) => {
+                        return (
+                          <Draggable
+                            key={item.goalSerialNumber}
+                            draggableId={item.goalSerialNumber}
+                            index={getInd(item.goalSerialNumber)}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                className="goal-container"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                style={getItemStyle(
+                                  snapshot.isDragging,
+                                  provided.draggableProps.style
+                                )}
+                              >
+                                <div {...provided.dragHandleProps}>
+                                  <DragOutlined />
+                                </div>
+                                <div className="name goal-name">
+                                  {item.title}
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        );
+                      })}
+                    </div>
+                  </>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
+
+export default AppGoal;
